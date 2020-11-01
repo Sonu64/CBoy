c4ce083adcc654ca09776586638cf31c3dab1e25
Sourakanti Mandal <52707495+Sonu64@users.noreply.github.com>
2020-11-01 12:31:25 +0530

    floats still not displaying correctly :(

--- dynamic_floats.c
+++ dynamic_floats.c
@@ -4,31 +4,44 @@ void sort(float*, int);
 
 int main()
 {
-    float *arr;
     int max, i;
-
+    float *arr;
     printf("Enter the number of elements in the float array: ");
     scanf("%d", &max);
     arr = (float *) malloc(max * sizeof(float));
     
     printf("Okay... Array created with %d elements, now enter those :) \n", max);
     
-
-    for(i = 0; i<max; i++)
+    for(i = 0; i < max; i++)
     {
-        if(i!=(max-1))
-            printf("%f, ", arr[i]);
-        else
-            printf("%f", arr[i]);
-    }    
-    sort(arr, max);
+        printf("Enter element number %d: ", i);
+        scanf("%f", &arr[i]);
+        printf("\n");
+    }
     
+    
+    printf("Initially - \n");
     for(i = 0; i<max; i++)
     {
-        if(i!=(max-1))
-            printf("%f, ", arr[i]);
+        if(i != (max-1))
+            printf("%.4f, ", arr[i]);
         else
-            printf("%f", arr[i]);
+            printf("%.4f", arr[i]);
+    }
+    printf("\n\n");
+    
+    
+    // SORTING //
+    sort(arr, max);
+   
+    
+    printf("Finally - \n");
+    for(i = 0; i<max; i++)
+    {
+        if(i != (max-1))
+            printf("%.4f, ", arr[i]);
+        else
+            printf("%.4f", arr[i]);
     }
     
     return 0;
@@ -43,7 +56,7 @@ void sort(float* arr, int max)
         {
             if(*(arr+i) > *(arr+j))
             {
-                temp = *(arr+i);
+                temp = *(arr+i); //arr[i] is same as *(arr+i),using this as we took address of zeroth element
                 *(arr+i) = *(arr+j);
                 *(arr+j) = temp;
             }
